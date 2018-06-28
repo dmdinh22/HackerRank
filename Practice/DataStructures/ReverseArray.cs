86d3690242073c666c994cabadee761db3129baa
David Dinh <dmdinh22@gmail.com>
2018-06-28 00:11:44 -0700

    using for loop

--- Practice/DataStructures/ReverseArray.cs
+++ Practice/DataStructures/ReverseArray.cs
@@ -21,5 +21,19 @@ namespace HackerRank.Practice.DataStructures
             Array.Reverse(a);
             return a;
         }
+
+        // using for loop
+        static int[] reverseArrayForLoop(int[] a)
+        {
+            for (int i = 0; i < a.Length / 2; i++)
+            {
+                int currentIndex = a[i];
+                a[i] = a[a.Length - i - 1];
+                a[a.Length - i - 1] = currentIndex;
+            }
+
+            return a;
+        }
+
     }
 }
