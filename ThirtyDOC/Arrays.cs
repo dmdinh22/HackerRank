3442cb6b8896bf8fc3e8414f41af2ea056de68f0
David Dinh <dmdinh22@gmail.com>
2018-07-03 00:19:25 -0700

    updated using reverse method

--- ThirtyDOC/Arrays.cs
+++ ThirtyDOC/Arrays.cs
@@ -10,7 +10,18 @@ namespace HackerRank.ThirtyDOC
 
             int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
 
+            // ## REVERSE FOR LOOP ##
+            /*
             for (var i = n - 1; i > -1; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+             */
+
+            // ## ARRAY REVERSE() METHOD
+            Array.Reverse(arr);
+
+            for (int i = 0; i < n; i++)
             {
                 Console.Write(arr[i] + " ");
             }
