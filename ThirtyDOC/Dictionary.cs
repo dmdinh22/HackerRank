25e68a0f97bd56d01fc276147db3d0091e5fdb05
David Dinh <dmdinh22@gmail.com>
2018-07-04 00:00:05 -0700

    dict and recursion

---
+++ ThirtyDOC/Dictionary.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HackerRank.ThirtyDOC
+{
+    public class Dictionary
+    {
+        static void DictionaryMain(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            Dictionary<string, int> phoneMap = new Dictionary<string, int>();
+            string input = Console.ReadLine();
+            int n = Convert.ToInt32(input);
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                string name = values[0];
+                int number = Convert.ToInt32(values[1]);
+
+                phoneMap.Add(name, number);
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                string check = Console.ReadLine();
+                if (phoneMap.ContainsKey(check))
+                {
+                    int output = phoneMap[check];
+                    Console.WriteLine("{0}={1}", check, output);
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+            }
+        }
+    }
+}
---
+++ ThirtyDOC/Recursion.cs
@@ -0,0 +1,45 @@
+using System;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class Recursion
+{
+
+    // Complete the factorial function below.
+    public static int factorial(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return (n * factorial(n - 1));
+        }
+    }
+
+    static void RecursionMain(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("
+                    OUTPUT_PATH "), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = factorial(n);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+}
