00d3147c8713b41b426f21873761d29ce693b6e0
David Dinh <dmdinh22@gmail.com>
2018-07-11 22:27:18 -0700

    more exceptions day 17

---
+++ ThirtyDOC/MoreExceptions.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Calculator{
+    public int power(int n, int p)
+    {
+        if(n < 0 || p < 0)
+        {
+            throw new Exception("n and p should be non-negative");
+        }
+        
+        return Convert.ToInt32(Math.Pow(n,p));
+    }  
+};
