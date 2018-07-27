e82bb7b9e76cb0c8170d42fb681b722d79b7acd8
David Dinh <dmdinh22@gmail.com>
2018-07-27 01:04:26 -0700

    removed comments from HR

--- ThirtyDOC/Testing.java
+++ ThirtyDOC/Testing.java
@@ -16,27 +16,23 @@ public class Solution {
 
     static class TestDataEmptyArray {
         public static int[] get_array() {
-            // complete this function
             return new int[0];
         }
     }
 
     static class TestDataUniqueValues {
         public static int[] get_array() {
-            // complete this function
             int a[] = {1, 2, 3};
             return a;
         }
 
         public static int get_expected_result() {
-            // complete this function
             return 0;
         }
     }
 
     static class TestDataExactlyTwoDifferentMinimums {
         public static int[] get_array() {
-            // complete this function
             int a[] = {1, 2, 1};
             return a;
         }
