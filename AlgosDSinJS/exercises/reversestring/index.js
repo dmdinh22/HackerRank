04f568f2da6100e7299b0e795e6d202bcaafe57a
David Dinh <dmdinh22@gmail.com>
2018-08-06 15:45:16 -0700

    using for loop for solution

--- AlgosDSinJS/exercises/reversestring/index.js
+++ AlgosDSinJS/exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// ## SOLUTION 2 ##
+function reverse(str) {
+  // create empty string called 'reversed'
+  let reversed = '';
+
+  // for each char in string
+  for (let char of str) {
+    //take char and add it to start of 'reversed' var
+    reversed = char + reversed;
+  }
+
+  // return var 'reversed
+  return reversed;
+}
+
+// ## SOLUTION 1 ##
+/*
 function reverse(str) {
   // // turn str into array
   // const arr = str.split('');
@@ -22,5 +39,6 @@ function reverse(str) {
     .reverse()
     .join('');
 }
+*/
 
 module.exports = reverse;
