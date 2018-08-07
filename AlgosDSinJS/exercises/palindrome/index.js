eb3614b2672909b6995c654aef67a9465e5fa7c6
David Dinh <dmdinh22@gmail.com>
2018-08-06 23:41:43 -0700

    palindrome solution

--- AlgosDSinJS/exercises/palindrome/index.js
+++ AlgosDSinJS/exercises/palindrome/index.js
@@ -7,6 +7,13 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  let reversedString = str
+    .split('')
+    .reverse()
+    .join('');
+
+  return str === reversedString ? true : false;
+}
 
 module.exports = palindrome;
