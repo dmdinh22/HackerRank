cd80a5af7405d00f9629ce138b1ff9b655c82776
David Dinh <dmdinh22@gmail.com>
2018-08-06 18:13:28 -0700

    third reverse string solution using reduce helper

--- AlgosDSinJS/exercises/reversestring/index.js
+++ AlgosDSinJS/exercises/reversestring/index.js
@@ -6,12 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// ## SOLUTION 3 ##
+// use reduce helper
+function reverse(str) {
+  // turn str into array
+  return (
+    str
+      .split('')
+      // use reduce helper method
+      // two args for reduce - arrow fn, starting arg value
+      // two args for arrow fn - first arg is reversed string,
+      // second is the element we're operating on
+      .reduce((reversed, character) => {
+        return character + reversed;
+      }, '')
+  ); // starting arg is empty string
+}
+
 // ## SOLUTION 2 ##
+/*
 function reverse(str) {
   // create empty string called 'reversed'
   let reversed = '';
 
-  // for each char in string
+  // for each char in string - new syntax (var X of arg)
   for (let char of str) {
     //take char and add it to start of 'reversed' var
     reversed = char + reversed;
@@ -20,6 +38,7 @@ function reverse(str) {
   // return var 'reversed
   return reversed;
 }
+*/
 
 // ## SOLUTION 1 ##
 /*
