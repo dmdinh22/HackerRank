c7c465e2c159c7c18dd720fadd0dd1dd6985f067
David Dinh <dmdinh22@gmail.com>
2018-08-10 01:55:01 -0700

    fizzbuzz

--- AlgosDSinJS/exercises/fizzbuzz/index.js
+++ AlgosDSinJS/exercises/fizzbuzz/index.js
@@ -12,6 +12,22 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  // start iterating at 1
+  for (let i = 1; i <= n; i++) {
+    // check if number is multiple of 3 AND 5
+    if (i % 15 === 0) {
+      console.log('fizzbuzz');
+      // is number a multiple of 3
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+      // is number a multiple of 5
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 module.exports = fizzBuzz;
