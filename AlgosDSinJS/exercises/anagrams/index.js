dc19f5106f4f6b16b7e97e3181a8f77a60a16ea1
David Dinh <dmdinh22@gmail.com>
2018-08-16 00:32:36 -0700

    anagram solution 2 using array helper method

--- AlgosDSinJS/exercises/anagrams/index.js
+++ AlgosDSinJS/exercises/anagrams/index.js
@@ -8,42 +8,55 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-// function anagrams(stringA, stringB) {
-// }
-
-// ## SOLUTION 1 ##
+// ## SOLUTION 2 ##
 function anagrams(stringA, stringB) {
-  // build char maps for strA and strB
-  const charMapA = buildCharMap(stringA);
-  const charMapB = buildCharMap(stringB);
-
-  // EDGE CASE - which string is longer by checking # of keys
-  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
-    return false;
-  }
-
-  // use FOR - IN when iterating over object
-  for (let char in charMapA) {
-    // check if the keys match
-    if (charMapA[char] !== charMapB[char]) {
-      return false;
-    }
-  }
-
-  return true;
+  //      check if it's equal (including type)
+  return cleanString(stringA) === cleanString(stringB);
 }
 
-// helper function to build char map
-buildCharMap = str => {
-  const charMap = {};
-
-  // iterate over str with regex cleanup
-  for (let char of str.replace(/[^\w]/g).toLowerCase()) {
-    // set charMap key as the current char we're on
-    charMap[char] = charMap[char]++ /* + 1 */ || 1;
-  }
-
-  return charMap;
+// helper function
+cleanString = str => {
+  return str
+    .replace(/[^\w]/g, '') // clean string of spaces and non chars
+    .toLowerCase()
+    .split('') // turn into array
+    .sort() // alphabetize
+    .join(''); // put back into array
 };
+
+// ## SOLUTION 1 ##
+// function anagrams(stringA, stringB) {
+//   // build char maps for strA and strB
+//   const charMapA = buildCharMap(stringA);
+//   const charMapB = buildCharMap(stringB);
+
+//   // EDGE CASE - which string is longer by checking # of keys
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//     return false;
+//   }
+
+//   // use FOR - IN when iterating over object
+//   for (let char in charMapA) {
+//     // check if the keys match
+//     if (charMapA[char] !== charMapB[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// // helper function to build char map
+// buildCharMap = str => {
+//   const charMap = {};
+
+//   // iterate over str with regex cleanup
+//   for (let char of str.replace(/[^\w]/g).toLowerCase()) {
+//     // set charMap key as the current char we're on
+//     charMap[char] = charMap[char]++ /* + 1 */ || 1;
+//   }
+
+//   return charMap;
+// };
 
 module.exports = anagrams;
