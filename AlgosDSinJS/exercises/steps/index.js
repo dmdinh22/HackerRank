18a43133cf327c962cd378d8ccaa13954f50d85d
David Dinh <dmdinh22@gmail.com>
2018-08-22 00:01:35 -0700

    iterative stair solution

--- AlgosDSinJS/exercises/steps/index.js
+++ AlgosDSinJS/exercises/steps/index.js
@@ -17,6 +17,28 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// ## SOLUTION 1 ##
+// Iterative
+function steps(n) {
+  // from 0 to n (iterate over rows)
+  for (let row = 0; row < n; row++) {
+    // create an empty string 'stair'
+    let stair = '';
+    // from 0 to n (iterate over columns)
+    for (let col = 0; col < n; col++) {
+      // IF current col <= current row
+      if (col <= row) {
+        // add # to 'stair'
+        stair += '#';
+        // ELSE
+      } else {
+        // add space to 'stair'
+        stair += ' ';
+      }
+    }
+    // console.log 'stair'
+    console.log(stair);
+  }
+}
 
 module.exports = steps;
